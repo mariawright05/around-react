7ac823278405bbbdea26752c172926ab3041ce1e
Maria Wright <maria@mariawrightstudios.com>
2020-11-22 23:09:21 -0500

    fix EditProfilePopup input fields

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm';
+import CurrentUserContext from '../contexts/CurrentUserContext';
+
 
 function EditProfilePopup(props) {
 
-  const [userName, setUserName] = React.useState('Name');
-  const [userAbout, setUserAbout] = React.useState('About');
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const [userName, setUserName] = React.useState(currentUser.name);
+  const [userAbout, setUserAbout] = React.useState(currentUser.about);
 
   const handleUserNameChange = (e) => {
     setUserName(e.target.value);
@@ -27,11 +31,11 @@ function EditProfilePopup(props) {
     <PopupWithForm name="edit-profile" title="Edit Profile" isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit}>
       <fieldset className="popup__info">
         <div className="popup__label">
-          <input id="profile-name" type="text" name="name" className="popup__field popup__field_type_name" placeholder={userName} required minLength="2" maxLength="40" onChange={handleUserNameChange} />
+          <input id="profile-name" type="text" name="name" className="popup__field popup__field_type_name" placeholder={"Name"} value={userName} required minLength="2" maxLength="40" onChange={handleUserNameChange} />
           <span id="profile-name-error" className="popup__error"></span>
         </div>
         <div className="popup__label">
-          <input id="profile-title" type="text" name="title" className="popup__field popup__field_type_title" placeholder={userAbout} required minLength="2" maxLength="200" onChange={handleUserAboutChange} />
+          <input id="profile-title" type="text" name="title" className="popup__field popup__field_type_title" placeholder={"About"} value={userAbout} required minLength="2" maxLength="200" onChange={handleUserAboutChange} />
           <span id="profile-title-error" className="popup__error"></span>
         </div>
       </fieldset>
